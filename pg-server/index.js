bf3e149f58223ee817ea6259c7d16eced348f84f
Sasmitha <143257185+Sasmitha05@users.noreply.github.com>
2024-08-29 21:24:38 +0530

    Update index.js

--- pg-server/index.js
+++ pg-server/index.js
@@ -115,6 +115,7 @@ app.get('/api/rooms', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
 });
